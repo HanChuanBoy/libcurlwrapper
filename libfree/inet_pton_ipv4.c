7a57c5c8c48acbfbbc66278d9d5969814b8587d2
hang.xu <zhiyanzhai563@163.com>
2018-05-14 08:11:04 +0800

    base inet api function

---
+++ libfree/inet_pton_ipv4.c
@@ -0,0 +1,32 @@
+/*
+	模拟网络编程
+*/
+int inet_pton(int family,const char*strptr,void*addrptr)
+{
+	if(family == AF_INET) {
+		struct in_addr in_val;
+		
+		if(inet_aton(strptr,&in_val)) {
+			memcpy(addrptr,&in_val,sizeof(struct in_addr));
+			return 1;
+		}
+		return 0;
+	}
+	errno=EAFNOSUPPORT;
+	return (-1);
+}
+
+const char*inet_ntop(int family,const void *addrptr,char*strptr,size_t len){
+	const u_char*p=(u_char*)addrptr;
+	if(family == AF_INET) {
+		char temp[INET_ADDRSTRLEN];
+		snprintf(temp,sizeof(temp),"%d.%d.%d.%d",p[0],p[1],p[2],p[3]);
+		if(strlen(temp) >=len){
+			errno=ENOSPC;
+			return NULL;
+		}
+		strcpy(strptr,temp);
+	}
+	errno=EAFNOSUPPORT;
+	return NULL;
+}
